83f215910684a29297ff69b12519820d3219245e
Donald Stufft <donald.stufft@gmail.com>
2011-08-03 23:36:56 -0400

    remove unneeded settings file

--- evilgenius_project/settings/paths.py
+++
@@ -1,5 +0,0 @@
-import os.path
-import pinax
-
-PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
