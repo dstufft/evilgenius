45ffc2e136c3d0fbdf50e4bf8ce24c7dd2e5829f
Donald Stufft <donald.stufft@gmail.com>
2011-08-08 17:35:52 -0400

    add prev/next links

--- evilgenius_project/apps/verbum/urls.py
+++ evilgenius_project/apps/verbum/urls.py
@@ -4,6 +4,8 @@ from verbum.views import VerbumIndexView, VerbumDetailView
 
 urlpatterns = patterns("",
     url(r"^$", VerbumIndexView.as_view(), name="verbum_index"),
+    url(r"^page(?P<page>[\d]+)/$", VerbumIndexView.as_view(), name="verbum_index_paged"),
+
     url(
         r"^(?P<year>[0-9]{4})/(?P<month>[\w]{3})/(?P<day>[0-3][0-9])/(?P<slug>[-\w]+)/$",
         VerbumDetailView.as_view(),
