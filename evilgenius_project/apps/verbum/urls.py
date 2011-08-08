b65548056b9bcb31748efc1c4aae0e6eec295ef4
Donald Stufft <donald.stufft@gmail.com>
2011-08-08 18:41:06 -0400

    fix the name of category url name

--- evilgenius_project/apps/verbum/urls.py
+++ evilgenius_project/apps/verbum/urls.py
@@ -7,7 +7,8 @@ urlpatterns = patterns("",
     url(r"^(?:page(?P<page>[\d]+)/)?$", VerbumIndexView.as_view(), name="verbum_index"),
 
     # Category Archive Views
-    url(r"^category/(?P<category>[\w]+)/$", CategoryIndexView.as_view(), name="verbum_category_index"),
+    url(r"^category/(?P<category>[\w]+)/$", CategoryIndexView.as_view(), name="verbum_category"),
+    url(r"^category/(?P<category>[\w]+)/page(?P<page>[\d]+)/$", CategoryIndexView.as_view(), name="verbum_category"),
 
     url(
         r"^(?P<year>[0-9]{4})/(?P<month>[\w]{3})/(?P<day>[0-3][0-9])/(?P<slug>[-\w]+)/$",
