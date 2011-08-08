cab3820f91cd1b6df663753674ca2d5a6a73f48d
Donald Stufft <donald.stufft@gmail.com>
2011-08-07 21:37:25 -0400

    allow pagination of verbum pages

--- evilgenius_project/apps/verbum/views.py
+++ evilgenius_project/apps/verbum/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import ArchiveIndexView
+
+from verbum import conf
 from verbum.models import Bloggable
 
 class VerbumIndexView(ArchiveIndexView):
 
     date_field = "when"
+    paginate_by = conf.PAGINATE_BY
     queryset = Bloggable.objects.filter(status=Bloggable.STATUS.published).select_subclasses()
