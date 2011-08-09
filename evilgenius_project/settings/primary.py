7b4ed1c4b990319229ec5bd6d748d6277eea2fe4
Donald Stufft <donald.stufft@gmail.com>
2011-08-08 21:14:10 -0400

    set db

--- evilgenius_project/settings/primary.py
+++ evilgenius_project/settings/primary.py
@@ -5,6 +5,7 @@ CACHES = {
         "BACKEND": "redis_cache.RedisCache",
         "LOCATION": "%s:%s" % (GONDOR_REDIS_HOST, GONDOR_REDIS_PORT),
         "OPTIONS": {
+            "DB": 1,
             "PASSWORD": GONDOR_REDIS_PASSWORD,
         },
     },
