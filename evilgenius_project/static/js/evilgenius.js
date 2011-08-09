3f8651c8c4d87e8457b970e775553f87c9d09948
Donald Stufft <donald.stufft@gmail.com>
2011-08-09 00:08:39 -0400

    update for new template

--- evilgenius_project/static/js/evilgenius.js
+++ evilgenius_project/static/js/evilgenius.js
@@ -1,10 +1,10 @@
 $(function(){
-    $("ul#social-media-icons li img").each(function(){
+    $("#social-media-icons ul li img").each(function(){
         var extension = $(this).attr("src").substring($(this).attr("src").lastIndexOf('.'));
         $(this).attr("src", $(this).attr("src").substring(0, $(this).attr("src").lastIndexOf('.')) + "-faded" + extension);
     });
 
-    $("ul#social-media-icons li img").hover(function(){
+    $("#social-media-icons ul li img").hover(function(){
         var extension = $(this).attr("src").substring($(this).attr("src").lastIndexOf('.'));
         $(this).attr("src", $(this).attr("src").substring(0, $(this).attr("src").lastIndexOf('-faded.')) + extension);
     }, function(){
